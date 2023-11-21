34b8e26bdb51576ec681ee8b1eeed24cc1105342
Ben Gosney <bengosney@googlemail.com>
2023-11-21 21:29:06 +0000

    Revert "Ensure SSL on CDN"
    This reverts commit 3fb5baf87d98ddc84467491a9863b465b4082f59.

--- aws.tf
+++ aws.tf
@@ -59,7 +59,7 @@ resource "aws_cloudfront_distribution" "cdn" {
     compress               = true
     smooth_streaming       = false
     target_origin_id       = local.s3_origin_id
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
     cache_policy_id        = local.cache_policy_CachingOptimized
   }
   ordered_cache_behavior {
